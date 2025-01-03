fc0f95ca3825b817d7fd8dee86735e1373da180d
Daniel Graham <dpgraham.85@gmail.com>
2025-01-03 14:46:48 -0800

    fix

--- modules/pipeline-runner/src/runner.js
+++ modules/pipeline-runner/src/runner.js
@@ -232,7 +232,7 @@ if (require.main === module) {
 
       executor = await buildPipeline(pipelineFile);
       executor.exitOnDone = program.opts().ci || process.env.CI;
-      
+
       // Watch for file changes
 
       // Initial run
