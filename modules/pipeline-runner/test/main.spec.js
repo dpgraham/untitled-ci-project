d7fd71b72f0ae851b2afd284796427e40d8cb8df
Daniel Graham <dpgraham.85@gmail.com>
2025-01-21 21:04:38 -0800

    fix problem with exec being called on paused containers

--- modules/pipeline-runner/test/main.spec.js
+++ modules/pipeline-runner/test/main.spec.js
@@ -1,12 +1,6 @@
 const { run } = require('../src/main');
 const path = require('path');
 
-// TODO: bug
-/*
-    When two jobs fail simultaneously (lint, unit-test) the job
-    exits twice
-*/
-
 describe('main', function () {
   describe('run', function () {
     test('run tiny.pipeine.js in CI mode', async function () {
