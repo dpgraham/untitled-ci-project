e8e6e3db1eb5bd9f57a5e841a316d0d8076870dd
Daniel Graham <dpgraham.85@gmail.com>
2025-02-17 16:32:30 -0800

    re-enable artifacts

--- node.pipeline.js
+++ node.pipeline.js
@@ -42,7 +42,7 @@ job('unit-test', () => {
   // TODO: 0
       //* investigate why artifacts failing in Github, reproduce using "act" local runner
       //* make it so that it does not fail when artifacts fails but rather gives a stern message
-  // artifacts('/ci/coverage');
+  artifacts('/ci/coverage');
   env('DOCKER_VERSION', '');
   // workdir('') // TODO: allow setting workdir here too
   copy('/ci'); // copies the files from the main container into this one
@@ -50,6 +50,7 @@ job('unit-test', () => {
   installNode();
   step('cd /ci');
   step('npm run test');
+  step('ls -lah /ci')
 });
 
 // job('commit-and-push', () => {
